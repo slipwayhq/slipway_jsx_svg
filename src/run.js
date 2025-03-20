66f1f90a1644f707977cf81b6f4c8818d5c56b67
James Thurley <james@thurley.com>
2025-03-20 06:54:39 +0000

    Migrate to exported JS run function

--- src/run.js
+++ src/run.js
@@ -1,30 +1,31 @@
-
 import satori, { React } from 'satori.js'
 
-let font_result = await slipway_host.font("Arial");
+export async function run(input) {
+  let font_result = await slipway_host.font("Arial");
 
-let font_data = new Uint8Array(font_result.data);
+  let font_data = new Uint8Array(font_result.data);
 
-let jsx_js = input.jsx_js;
-let converted_jsx = eval(jsx_js);
+  let jsx_js = input.jsx_js;
+  let converted_jsx = eval(jsx_js);
 
-const svg = await satori(
-  converted_jsx,
-  {
-    width: input.width,
-    height: input.height,
-    embedFont: true,
-    fonts: [
-      {
-        name: 'sans-serif',
-        data: font_data.buffer,
-        weight: 400,
-        style: 'normal',
-      },
-    ],
-  },
-);
+  const svg = await satori(
+    converted_jsx,
+    {
+      width: input.width,
+      height: input.height,
+      embedFont: true,
+      fonts: [
+        {
+          name: 'sans-serif',
+          data: font_data.buffer,
+          weight: 400,
+          style: 'normal',
+        },
+      ],
+    },
+  );
 
-export let output = {
-  svg: svg,
-};
+  return {
+    svg: svg,
+  };
+}
